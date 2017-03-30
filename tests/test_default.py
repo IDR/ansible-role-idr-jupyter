b140ed3ce40461c955c427d3a9b23f423ec155e6
Simon Li <spli@dundee.ac.uk>
2017-03-30 17:58:57 +0100

    GitHub Oauth, check membership of organisation

---
+++ tests/test_default.py
@@ -0,0 +1,24 @@
+import testinfra.utils.ansible_runner
+import pytest
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
+
+
+def test_services_running_and_enabled(Service):
+    assert Service('docker-jupyterhub').is_running
+    assert Service('docker-jupyterhub').is_enabled
+
+
+@pytest.mark.parametrize("password,status", [
+    ("ome", True),
+    ("incorrect", False),
+])
+def test_login(Command, password, status):
+    out = Command.check_output(
+        'curl -L --data %s %s',
+        'username=user&password=%s&submit=Sign+In' % password,
+        'http://localhost:8000/jupyter/hub/login?next=')
+    assert ('login_error' not in out) == status
+
+# TODO: check notebook container runs after login
